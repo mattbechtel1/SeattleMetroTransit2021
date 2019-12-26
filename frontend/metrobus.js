dfef5fd78e1014c5b6235320e8f221844034ee22
mattbechtel1 <bechma02@gmail.com>
2019-12-26 18:31:59 -0500

    minor styling added to bus table

--- frontend/metrobus.js
+++ frontend/metrobus.js
@@ -1,16 +1,13 @@
-var myKey = config.PRIMARY_KEY;
 const busPredictionPrefix = 'https://api.wmata.com/NextBusService.svc/json/jPredictions/?StopID='
-const configObj = {
-    method: 'GET',
-    headers: {
-        api_key: myKey
-    }
-}
 const alarmSound = new Audio('./assets/charge.wav');
+const confirmSound = new Audio('./assets/263133__pan14__tone-beep.wav');
+const lostSound = new Audio('./assets/159408__noirenex__life-lost-game-over.wav');
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('search-by-stop').addEventListener('click', () => popUpSearch(stopSearch));
     document.getElementById('search-by-route').addEventListener('click', () => popUpSearch(routeSearch));
+    // login/out button
+    // favorites button
 })
 
 function popUpSearch(searchFunction) {
@@ -20,11 +17,12 @@ function popUpSearch(searchFunction) {
     const form = document.createElement('form');
     form.id = 'search-form'
     const input = document.createElement('input');
-    input.classList.add('input')
+    input.classList.add('input');
     input.setAttribute('name', 'queryData');
     input.setAttribute('maxlength', '7')
     input.setAttribute('placeholder', "Enter 7-digit stop # or route")
     const submitBtn = document.createElement('button');
+    submitBtn.classList.add('button', 'is-primary')
     submitBtn.innerText = 'Search';
     submitBtn.type = 'submit';
 
@@ -53,7 +51,6 @@ function stopSearch(event) {
     .then(data => getBuses(data, stopId))
 }
 
-
 function routeSearch() {
     alert("Route search is not yet supported. Please try searching by stop")
 }
@@ -72,8 +69,17 @@ function getBuses(data, stopId) {
 
         data.Predictions.forEach(function(prediction) {
             let row = document.createElement('tr');
+            
             let busNum = document.createElement('td');
-            busNum.innerText = prediction.RouteID + " " + prediction.DirectionText;
+                const busBox = document.createElement('div');
+                busBox.classList.add('busbox')
+                busBox.innerText = prediction.RouteID;
+                
+                const busDesc = document.createElement('div')
+                busDesc.classList.add('bus-description')
+                busDesc.innerText = " " + prediction.DirectionText;
+                busNum.append(busBox, busDesc)
+
             let busMinutes = document.createElement('td');
             busMinutes.innerText = prediction.Minutes + ' minutes';
             let alarmSet = document.createElement('a');
@@ -100,20 +106,27 @@ function askAlarm(event) {
     let stopId = event.target.dataset.stop;
 
     let internalForm = document.createElement('form');
+
+    let internalInputLabel = document.createElement('label')
+    internalInputLabel.classList.add('label')
+    internalInputLabel.innerText = "Set reminder for number of minutes before departure:"
     let internalInput = document.createElement('input');
+    internalInput.classList.add('input', 'is-primary')
     internalInput.setAttribute('type', 'number');
     internalInput.setAttribute('name', 'alarm');
     internalInput.setAttribute('min', '1');
     internalInput.setAttribute('max', event.target.dataset.minutes)
     let internalSubmit = document.createElement('button');
+    internalSubmit.classList.add('button', 'is-primary')
     internalSubmit.type = 'submit';
     internalSubmit.innerText = "Set Alarm"
     internalForm.addEventListener('submit', function(e) {setAlarm(e, stopId, tripId)})
     let internalClose = document.createElement('button');
+    internalClose.classList.add('button', 'is-primary')
     internalClose.innerText = "Close"
     internalClose.addEventListener('click', closeAlarmDrawer)
 
-    internalForm.append(internalInput, internalSubmit, internalClose)
+    internalForm.append(internalInputLabel, internalInput, internalSubmit, internalClose)
     newTd.appendChild(internalForm)
     tripContainer.parentNode.appendChild(newTr)
     tripContainer.after(newTr)
@@ -132,7 +145,11 @@ function setAlarm(event, stopId, tripId) {
     notificationBlock.classList.add('notification', 'is-success');
     let closeBtn = document.createElement('button')
     closeBtn.classList.add('delete')
+    closeMarker = document.createElement('i')
+    closeMarker.classList.add('fas', 'fa-times')
+    closeBtn.appendChild(closeMarker)
     notificationBlock.appendChild(closeBtn);
+    confirmSound.play()
     notificationBlock.innerText = `An alarm has been set for bus #${tripId} at stop #${stopId} with ${alarmSettingMins} minutes' warning.`
     
     let checkAlarm = function(busPredictions) {
@@ -148,8 +165,9 @@ function setAlarm(event, stopId, tripId) {
         }
 
         if (!myBus) {
+            lostSound.play()
             alert("We're sorry. Your selected bus is no longer providing prediction data.")
-            // clearInterval(alarm)
+            notificationBlock.innerHTML = ""
         }
     };
     
