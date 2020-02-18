dcde207909eebcf3cadf05772deda4e954f1ac2c
mattbechtel1 <bechma02@gmail.com>
2020-02-18 12:46:29 -0500

    removed config js from gitignore

---
+++ config.js
@@ -0,0 +1,44 @@
+const favoriteUrl = 'http://localhost:3000/favorites'
+
+const hostedObj = function(requestType, formResponseObj) {
+  return {
+    method: requestType,
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(formResponseObj)
+  }
+}
+
+function clearAndReturnMain() {
+  let mainContainer = document.getElementById('main-container')
+  mainContainer.innerHTML = ""
+  return mainContainer
+}
+
+function addFavorite(e) {
+  if (!userHeldInState) {
+    errorNotification('Please log in to declare favorites')
+
+  } else {
+
+    let stopId = e.target.dataset.stop;
+    let defaultDescription = e.target.dataset.description
+    
+    let postObj = {
+      'lookup': stopId,
+      'user_id': userHeldInState.id,
+      'description': defaultDescription,
+      'transit_type': e.target.dataset.stopType,
+      'permanent_desc': defaultDescription
+    }
+    
+    fetch(favoriteUrl, hostedObj('POST', postObj))
+    .then(response => response.json())
+    .then(newFav => {
+      userHeldInState.favorites.push(newFav)
+      confirmNotification(`${newFav.description} has been added to your favorites.`)
+    })
+  }
+}
