ac59c1da74216983d75825a5b8be227afef694fe
mattbechtel1 <bechma02@gmail.com>
2020-04-02 13:37:06 -0400

    redis integration completed

--- backend/app/controllers/metro_controller.rb
+++ backend/app/controllers/metro_controller.rb
@@ -14,7 +14,7 @@ class MetroController < ApplicationController
   def bus_stop
     unless Redis.current.exists("stop-#{params[:stop_id]}")
       response = fetch_data("https://api.wmata.com/NextBusService.svc/json/jPredictions/?StopID=#{params[:stop_id]}", nil)
-      Redis.current.set("stop-#{params[:stop_id]}", response, {ex: 604800})
+      Redis.current.set("stop-#{params[:stop_id]}", response, {ex: 15})
     end
   
     render json: Redis.current.get("stop-#{params[:stop_id]}")
@@ -30,21 +30,30 @@ class MetroController < ApplicationController
   end
 
   def stations
-    response = fetch_data_with_params('https://api.wmata.com/Rail.svc/json/jStations', params, nil)
+    unless Redis.current.exists('allStations')
+      response = fetch_data_with_params('https://api.wmata.com/Rail.svc/json/jStations', params, nil)
+      Redis.current.set('allStations', response, {ex: 604800})
+    end
 
-    render json: response
+    render json: Redis.current.get('allStations')
   end
 
   def station
-    response = fetch_data("https://api.wmata.com/StationPrediction.svc/json/GetPrediction/#{params[:station_code]}", nil)
+    unless Redis.current.exists("station-#{params[:station_code]}")
+      response = fetch_data("https://api.wmata.com/StationPrediction.svc/json/GetPrediction/#{params[:station_code]}", nil)
+      Redis.current.set("station-#{params[:station_code]}", response, {ex: 20})
+    end
 
-    render json: response
+    render json: Redis.current.get("station-#{params[:station_code]}")
   end
 
   def lines
-    response = fetch_data('https://api.wmata.com/Rail.svc/json/jLines', nil)
+    unless Redis.current.exists("lines")
+      response = fetch_data('https://api.wmata.com/Rail.svc/json/jLines', nil)
+      Redis.current.set('lines', response)
+    end
 
-    render json: response
+    render json: Redis.current.get('lines')
   end
 
   private
