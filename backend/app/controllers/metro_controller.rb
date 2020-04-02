f810ed1a8f2c55d8cdf763a469454ffda2674088
mattbechtel1 <bechma02@gmail.com>
2020-04-02 12:48:25 -0400

    updated ruby version for project and integrated redis for bus controllers

--- backend/app/controllers/metro_controller.rb
+++ backend/app/controllers/metro_controller.rb
@@ -1,21 +1,26 @@
 require 'net/http'
-require 'JSON'
 
 class MetroController < ApplicationController
   def bus_stops
-    response = fetch_data_with_params('https://api.wmata.com/Bus.svc/json/jRouteSchedule', params, nil)
+    
+    unless Redis.current.exists("busstops-#{params[:RouteID]}")
+      response = fetch_data_with_params('https://api.wmata.com/Bus.svc/json/jRouteSchedule', params, nil)
+      Redis.current.set("busstops-#{params["RouteID"]}", response, {ex: 604800})
+    end
 
-    render json: response
+    render json: Redis.current.get("busstops-#{params[:RouteID]}")
   end
 
   def bus_stop
-    response = fetch_data("https://api.wmata.com/NextBusService.svc/json/jPredictions/?StopID=#{params[:stop_id]}", nil)
+    unless Redis.current.exists("stop-#{params[:stop_id]}")
+      response = fetch_data("https://api.wmata.com/NextBusService.svc/json/jPredictions/?StopID=#{params[:stop_id]}", nil)
+      Redis.current.set("stop-#{params[:stop_id]}", response, {ex: 604800})
+    end
   
-    render json: response
+    render json: Redis.current.get("stop-#{params[:stop_id]}")
   end
 
   def bus_route_list
-
     unless Redis.current.exists('allBuses')
       response = fetch_data('https://api.wmata.com/Bus.svc/json/jRoutes', nil)
       Redis.current.set('allBuses', response, {ex: 604800})
--- backend/config/initializers/redis.rb
+++ backend/config/initializers/redis.rb
@@ -3,6 +3,6 @@ require 'redis'
 Redis.current = Redis.new(
     url: Figaro.env.redis_url,
     port: Figaro.env.redis_port,
-    password: Figaro.env.redis_password,
+    # password: Figaro.env.redis_password,
     db: Figaro.env.redis_db
 )
