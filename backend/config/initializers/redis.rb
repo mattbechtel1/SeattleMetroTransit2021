11d7a510239ad6cfeb0e00a02aca8b2496b656a4
mattbechtel1 <bechma02@gmail.com>
2020-04-02 21:54:21 -0400

    changed initializer for production

--- backend/config/initializers/redis.rb
+++ backend/config/initializers/redis.rb
@@ -1,6 +1,6 @@
 Redis.current = Redis.new(
-    url: Figaro.env.redis_url,
-    port: Figaro.env.redis_port,
-    # password: Figaro.env.redis_password,
-    db: Figaro.env.redis_db
+    url: ENV["REDIS_URL"],
+    port: ENV["REDIS_PORT"],
+    # password: Figaro.env.redis_password, 
+    db: ENV["REDIS_DB"]
 )
