04163de458070ead9f242a29c8616a7d49211fa8
Matthew Bechtel <bechma02@gmail.com>
2021-11-02 21:34:13 -0700

    Adding has_many attributions

--- backend/app/models/agency.rb
+++ backend/app/models/agency.rb
@@ -1,3 +1,6 @@
 class Agency < ApplicationRecord
     acts_as_copy_target
+    has_many :fares, class_name: "FareAttribute"
+    has_many :routes
+    has_many :trips, through: :routes
 end
--- backend/app/models/route.rb
+++ backend/app/models/route.rb
@@ -1,4 +1,7 @@
 class Route < ApplicationRecord
   acts_as_copy_target
   belongs_to :agency
+  has_many :trips
+  has_many :route_fares
+  has_many :fares, through: :route_fares, source: :fare_attribute
 end
