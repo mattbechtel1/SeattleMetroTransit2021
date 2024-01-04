a626d54602696184dfdf32438518cd56a46655eb
Matthew Bechtel <bechma02@gmail.com>
2024-01-03 20:00:03 -0800

    remove circulator from routes file

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -8,10 +8,6 @@ Rails.application.routes.draw do
   get 'metro/lines'
   get 'metro/alerts'
 
-  get 'circulator/busstops/:r', to: 'circulator#bus_stops'
-  get 'circulator/busstop/:stopId', to: 'circulator#bus_stop'
-  get 'circulator/busroutes', to: 'circulator#bus_route_list'
-
   resources :favorites, only: [:create, :destroy, :update]
   resources :users, only: [:create]
   get 'login', to: 'sessions#new'
