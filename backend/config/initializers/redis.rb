ad56471f6b1891922275e6b62d14f2c9fd6b4864
Matthew Bechtel <bechma02@gmail.com>
2025-07-27 16:57:50 -0700

    Fix redis initializer syntax bug

--- backend/config/initializers/redis.rb
+++ backend/config/initializers/redis.rb
@@ -2,6 +2,6 @@ $redis = Redis.new(
     url: ENV["REDIS_URL"],
     port: ENV["REDIS_PORT"],
     # password: Figaro.env.redis_password, 
-    db: ENV["REDIS_DB"]
+    db: ENV["REDIS_DB"],
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
 )
