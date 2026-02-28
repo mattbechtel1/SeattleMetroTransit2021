a3f9a22b216c6190276bfd1fa2643abb25edb2ec
Matthew Bechtel <bechma02@gmail.com>
2026-02-28 11:57:21 -0800

    add browser open step to start script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start PHP server for frontend on port 8001
-php -S localhost:8001 -t frontend frontend/index.php &
+(cd frontend && php -S localhost:8001) &
 PHP_PID=$!
 
 # Start Rails server for backend on port 3000
@@ -11,6 +11,13 @@ RAILS_PID=$!
 echo "PHP server running on http://localhost:8001 (PID $PHP_PID)"
 echo "Rails server running on http://localhost:3000 (PID $RAILS_PID)"
 echo "Press Ctrl+C to stop both servers."
+
+sleep 1
+if command -v xdg-open &>/dev/null; then
+    xdg-open http://localhost:8001
+elif command -v open &>/dev/null; then
+    open http://localhost:8001
+fi
 
 trap "kill $PHP_PID $RAILS_PID" EXIT
 wait
